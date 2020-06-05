84507a6c1652c2e8984bed64857dd5aef538637f
chasewri <chasewri@gmail.com>
2020-06-05 17:09:54 -0600

    oauth start

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ require('./config/passport');
 
 // ROUTES
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/recipes');
 
 
 var app = express();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var indexCtrl = require('../controllers/index');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Per Se Recipes', heroId: 'index-hero' });
+router.get('/', isLoggedIn, indexCtrl.index);
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/',
+    failureRedirect : '/'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
+
+function isLoggedIn(req, res, next) {
+  if ( req.isAuthenticated() ) return next();
+  res.redirect('/auth/google');
+}
+
 
 module.exports = router;

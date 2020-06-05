558bc55b0138ee7bfbecc5febb52e1c9c2239c9c
chasewri <chasewri@gmail.com>
2020-06-05 16:26:44 -0600

    routes work

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -5,8 +5,8 @@ const recipeSchema = new Schema({
     updated: { type: Date, default: Date.now },
     name: String,
     imgUrl: String,
-    imgThumb: String,
     recipe: String,
+    description: String,
     category: {
         type: Schema.Types.ObjectId,
         ref: 'Category'

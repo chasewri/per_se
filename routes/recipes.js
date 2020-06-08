de3b3f4320615af41f0b9ae487feea8167cf7fab
chasewri <chasewri@gmail.com>
2020-06-08 08:46:01 -0600

    update routes

--- routes/recipes.js
+++ routes/recipes.js
@@ -8,7 +8,7 @@ router.get("/myRecipes", isLoggedIn, recipesCtrl.myRecipes);
 router.get("/new", isLoggedIn, recipesCtrl.new);
 router.post("/add", isLoggedIn, recipesCtrl.create);
 router.get("/categories", recipesCtrl.cats);
-router.get("/:id", recipesCtrl.show);
+router.get("/:id", isLoggedIn, recipesCtrl.show);
 router.get("/:id/edit", isLoggedIn, recipesCtrl.edit);
 router.get("/categories/:id", recipesCtrl.catShow);
 router.put("/:id", isLoggedIn, recipesCtrl.update);

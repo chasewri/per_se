68fdc3d46cb660fa55080cf72d46060190c7b916
chasewri <chasewri@gmail.com>
2020-06-08 05:42:14 -0600

    add delete functionality

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,56 +1,90 @@
-const User = require('../models/userModel');
-const Recipe = require('../models/recipeModel');
-const Category = require('../models/categoryModel')
+const User = require("../models/userModel");
+const Recipe = require("../models/recipeModel");
+const Category = require("../models/categoryModel");
 
 module.exports = {
-    index,
-    new: newRecipe,
-    myRecipes,
-    create,
-    show,
-    
+  index,
+  new: newRecipe,
+  myRecipes,
+  create,
+  show,
+  edit,
+  update,
+  delete: deleteOne,
+};
+
+function deleteOne(req, res) {
+  Recipe.findByIdAndDelete(req.params.id, function (err) {
+    res.redirect("/recipes/myrecipes");
+  });
 }
-function show(req, res) {
-    Recipe.findById(req.params.id, function(err, recipe) {
-        res.render('recipes/show', {
-            user: req.user,
-            recipe: recipe
-        });
+
+function edit(req, res) {
+  Recipe.findById(req.params.id, function (err, recipe) {
+    Category.find({}, function (err, categories) {
+      res.render("recipes/update", {
+        user: req.user,
+        recipe: recipe,
+        title: `Edit ${recipe.name}`,
+        categories: categories,
+      });
     });
+  });
+}
+
+function update(req, res) {
+  Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (
+    err
+  ) {
+    res.redirect("/recipes/myrecipes");
+  });
+}
+
+function show(req, res) {
+  Recipe.findById(req.params.id, function (err, recipe) {
+    res.render("recipes/show", {
+      user: req.user,
+      recipe: recipe,
+    });
+  });
 }
 
 function newRecipe(req, res) {
-    Category.find({}, function(err, categories) {  
-        res.render('recipes/new', 
-        { 
-            user: req.user,
-            title: 'Create a New Recipe',
-            categories: categories
-        })
-    })
+  Category.find({}, function (err, categories) {
+    res.render("recipes/new", {
+      user: req.user,
+      title: "Create a New Recipe",
+      categories: categories,
+    });
+  });
 }
 function index(req, res) {
-    Recipe.find({}, function(err, recipes) {
-        res.render('recipes/index', { user: req.user, recipes: recipes });
-    })
+  Recipe.find({})
+  .sort({ updatedAt: -1 })
+  .exec(function(err, recipes) {
+    res.render("recipes/index", {
+       user: req.user, 
+       recipes: recipes,
+       title: 'Newest Recipes' 
+      });
+  });
 }
 function myRecipes(req, res) {
-    Recipe.find({author: req.user._id}, function(err, recipes) {
-        res.render('recipes/myRecipes', { 
-            user: req.user, 
-            recipes: recipes,
-            title: 'My Recipe Collection' 
-        });
-    })
+  Recipe.find({ author: req.user._id }, function (err, recipes) {
+    res.render("recipes/myRecipes", {
+      user: req.user,
+      recipes: recipes,
+      title: "My Recipe Collection",
+    });
+  });
 }
 function create(req, res) {
-    req.body.author = req.user._id;
-    req.body.authorName = req.user.name;
-    const newRecipe = new Recipe(req.body);
-    newRecipe.save(function(err) {
-        if (err) return res.redirect('/recipes/new');
-        console.log(newRecipe);
-        res.redirect('/recipes/myRecipes');
-    })
-
+  req.body.author = req.user._id;
+  req.body.authorName = req.user.name;
+  const newRecipe = new Recipe(req.body);
+  newRecipe.save(function (err) {
+    if (err) return res.redirect("/recipes/new");
+    console.log(newRecipe);
+    res.redirect("/recipes/myRecipes");
+  });
 }
--- routes/recipes.js
+++ routes/recipes.js
@@ -3,11 +3,14 @@ var router = express.Router();
 var recipesCtrl = require('../controllers/recipes');
 
 // you're in recipes here
-router.get('/', isLoggedIn, recipesCtrl.index);
+router.get('/', recipesCtrl.index);
 router.get('/myRecipes', isLoggedIn, recipesCtrl.myRecipes);
 router.get('/new', isLoggedIn, recipesCtrl.new);
 router.post('/add', isLoggedIn, recipesCtrl.create);
 router.get('/:id', recipesCtrl.show);
+router.get('/:id/edit', isLoggedIn, recipesCtrl.edit);
+router.put('/:id', isLoggedIn, recipesCtrl.update);
+router.delete('/:id', isLoggedIn, recipesCtrl.delete);
 
 function isLoggedIn(req, res, next) {
   if ( req.isAuthenticated() ) return next();

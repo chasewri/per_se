cd75834b567243ae8312e9522ce5d155ea399370
chasewri <chasewri@gmail.com>
2020-06-09 07:27:03 -0600

    mobile view fix

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ require("./config/passport");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var recipeRouter = require("./routes/recipes");
+var categoryRouter = require("./routes/category");
 
 var app = express();
 
@@ -48,6 +49,7 @@ app.use(function (req, res, next) {
 app.use("/", indexRouter);
 app.use("/", usersRouter);
 app.use("/recipes", recipeRouter);
+// app.use('/categories', categoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

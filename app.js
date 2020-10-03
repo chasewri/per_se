7e91a45d3a7b147acbb465002d705764280b1d02
chasewri <chasewri@gmail.com>
2020-10-03 12:38:11 -0600

    add helmet

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var helmet = require("helmet");
 var logger = require("morgan");
 var session = require("express-session");
 var passport = require("passport");
+// var redisClient = require("redis").createClient();
 var methodOverride = require("method-override");
 
 require("dotenv").config();
@@ -16,7 +17,8 @@ require("./config/passport");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var recipeRouter = require("./routes/recipes");
-var categoryRouter = require("./routes/category");
+// var categoryRouter = require("./routes/category");
+const { lookup } = require("dns");
 
 var app = express();
 
@@ -24,8 +26,21 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// app.use(helmet());
-app.disable("x-powered-by");
+// var limiter = require("express-limiter")(app, redisClient);
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "cdn.jsdelivr.net"],
+        imgSrc: ["'self'", "i.imgur.com"],
+        styleSrc: ["'self'", "cdn.jsdelivr.net"]
+      },
+    },
+  })
+);
+// app.disable("x-powered-by");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -40,6 +55,12 @@ app.use(
     saveUninitialized: true,
   })
 );
+
+// limiter({
+//   lookup: ["connection.remoteAddress"],
+//   total: 100,
+//   expire: 1000 * 60 * 60,
+// });
 
 app.use(passport.initialize());
 app.use(passport.session());
